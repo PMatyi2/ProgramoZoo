f69b89d344de11027451438b57b47c59e7ce0b33
PMatyi2 <palotai.matyas@gmail.com>
2019-09-27 11:27:26 +0200

    Add files via upload

---
+++ js/scrolling-nav.js
@@ -0,0 +1,51 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+/*
+//browser window scroll (in pixels) after which the "back to top" link is shown
+var offset = 300,
+    //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+    offset_opacity = 1200,
+    //duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700;*/
+
+/*This makes things appear when scrolling down*/
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".footer").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) {
+                $('.footer').fadeIn();
+            } else {
+                $('.footer').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
